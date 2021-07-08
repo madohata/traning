ba01dc26c126ca72dc42ad665d55d9867460041c
madohata <4444ffffoota@gmail.com>
2021-07-08 14:20:22 +0900

    コメントを追加

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+// コメントを追加
 public class HelloWorld {
  public static void main(String[] args) {
   System.out.println("Hello World!");
  }
 }
+
+
+// あああああああああああああああああああああああああああああああああああ
+// あああああああああああああああああああああああああああああああああああ
+// 鮪鮪鮪鮪鮪鮪鮪鮪鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨鮨
